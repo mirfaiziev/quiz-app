f41a4e6b6dab902bbb05bcd6c30186810083e338
Victor <mirfaiziev.victor@gmail.com>
2024-03-21 20:47:12 +0100

    add result calculation

--- frontend/app/api/result/route.ts
+++ frontend/app/api/result/route.ts
@@ -4,8 +4,26 @@ async function POST(
   request: Request
 ) {
   const data = await request.json()
-  console.log(data)
-  return NextResponse.json({a:"B"})
+  let valuesCount = {} as any // todo
+
+  for(const answerId in data) {
+    if (data[answerId] in valuesCount) {
+      valuesCount[data[answerId]] = valuesCount[data[answerId]] + 1
+    } else {
+      valuesCount[data[answerId]] = 1
+    }
+  }
+
+  let maxCountValue = null
+  let maxCount = 0
+  for (const value in valuesCount) {
+    if (maxCount < valuesCount[value]) {
+      maxCount = valuesCount[value]
+      maxCountValue = value
+    }
+  }
+
+  return NextResponse.json({type:maxCountValue})
 }
 
 export {POST}
